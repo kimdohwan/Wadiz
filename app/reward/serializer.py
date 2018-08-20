197bdfc15afebc3e78beec16864b449c81db1311
dohwan <dosio0102@gmail.com>
2018-08-21 02:22:36 +0900

    User 의 funding list serializer 구현

--- app/reward/serializer.py
+++ app/reward/serializer.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from members.serializer import UserSerializer
 from reward.models import Product, Reward, ProductLike, Funding
 
 User = get_user_model()
